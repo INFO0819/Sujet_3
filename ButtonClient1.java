77cd09d6edfd21c69323d1feab2d5aca98f7c23e
Joffrey Herard <joffrey.herard@etudiant.univ-reims.fr>
2017-02-21 12:08:46 +0100

    Chaine de transmission

--- ButtonClient1.java
+++ ButtonClient1.java
@@ -94,9 +94,10 @@ public class ButtonClient1 extends JButton implements MouseListener {
 		
 		}	
 	}
-	
+	//ok pe
 	public void Client1(){
 		 try {
+			  System.out.println("CLIENT 1 ");
 			  String a_envoyer ="1,Bonjour test1";  
 		      InetAddress serveur = InetAddress.getByName("localhost");
 		      int length = a_envoyer.length();
@@ -106,13 +107,30 @@ public class ButtonClient1 extends JButton implements MouseListener {
 		      DatagramPacket donneesRecues = new DatagramPacket(new byte[taille], taille);
 
 		      socket.setSoTimeout(30000);
-		      socket.send(donneesEmises);
+		      socket.send(donneesEmises);	
+		      System.out.println("On attend confirmation");
 		      socket.receive(donneesRecues);
 
-		      System.out.println("Message : " + new String(donneesRecues.getData(), 
-		        0, donneesRecues.getLength()));
-		      System.out.println("de : " + donneesRecues.getAddress() + ":" + 
-		        donneesRecues.getPort());
+		      System.out.println("Message : " + new String(donneesRecues.getData(), 0, donneesRecues.getLength()));
+		      System.out.println("de : " + donneesRecues.getAddress() + ":" + donneesRecues.getPort());
+		     
+		      // effectuer envoie apres confirmation pret 
+		    
+		      a_envoyer="1,Message de test transmission venant  de 1 ";
+		      length = a_envoyer.length();
+		      buffer = a_envoyer.getBytes();
+		      donneesEmises = new DatagramPacket(buffer,length, serveur, port);
+		      socket.send(donneesEmises);
+			  /*On attend retour de 3 */
+		      System.out.println("On attend le retour du 3 ");
+		      donneesRecues = new DatagramPacket(new byte[taille], taille);  
+		      socket.receive(donneesRecues);
+		      
+		      System.out.println("Message : " + new String(donneesRecues.getData(), 0, donneesRecues.getLength()));
+		      System.out.println("de : " + donneesRecues.getAddress() + ":" + donneesRecues.getPort());
+		      
+		      System.out.println("Terminaison  ");   
+				      
 		    } catch (SocketTimeoutException ste) {
 		      System.out.println("Le delai pour la reponse a expire");
 		    } catch (Exception e) {
@@ -133,41 +151,127 @@ public class ButtonClient1 extends JButton implements MouseListener {
 		    String donnees = "";
 		    String message = "";
 		    int taille = 0;
-		
+		    
+		    String donnees2 = "";
+		    String message2 = "";
+		    int taille2 = 0;
+		    
 		    System.out.println("Lancement du serveur");
-		    while (true) {
-		      DatagramPacket paquet = new DatagramPacket(buffer, buffer.length);
-		      DatagramPacket envoi = null;
-		      try {
-				socket.receive(paquet);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-		      System.out.println("\n"+paquet.getAddress());
-		      taille = paquet.getLength();
-		      donnees = new String(paquet.getData(),0, taille);
-		      System.out.println("Donnees reçues = "+donnees);
-		
-		      message = "Bonjour "+donnees;
-		      System.out.println("Donnees envoyees = "+message);
-		      envoi = new DatagramPacket(message.getBytes(), 
-		        message.length(), paquet.getAddress(), paquet.getPort());
-		      try {
-				socket.send(envoi);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		    /*On attend les deux client */
+		    DatagramPacket paquet = new DatagramPacket(buffer, buffer.length);
+		    DatagramPacket paquet2 = new DatagramPacket(buffer, buffer.length);
+		    DatagramPacket paquet3 = new DatagramPacket(buffer, buffer.length);
+		    
+		    DatagramPacket envoi = null;
+		    try {
+			  socket.receive(paquet);
+		    } catch (IOException e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
 		    }
+		    // message du premier client 1 
+		    System.out.println("\n"+paquet.getAddress());
+		    taille = paquet.getLength();
+		    donnees = new String(paquet.getData(),0, taille);
+		    System.out.println("Donnees reçues = "+donnees);
+		  
+		  
+		   try {
+			 	socket.receive(paquet2);
+		   }catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		   }
+		   //message du deuxieme client
+		   System.out.println("\n"+paquet2.getAddress());
+		   taille2 = paquet2.getLength();
+		   donnees2 = new String(paquet2.getData(),0, taille);
+		   System.out.println("Donnees reçues = "+donnees2);
+		   // On envoie la confirmation 
+		   
+		    String a_envoyer2 ="OK";  
+		    envoi = new DatagramPacket(a_envoyer2.getBytes(), a_envoyer2.length(), paquet.getAddress(), paquet.getPort());
+		    System.out.println("On envoie le OK 1 ");
+			try {
+				socket.send(envoi);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			 
+		    envoi = new DatagramPacket(a_envoyer2.getBytes(), 
+				  a_envoyer2.length(), paquet2.getAddress(), paquet2.getPort());
+		    System.out.println("On envoie le ok 2 ");
+			try {
+				socket.send(envoi);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		    while (true) {
+		    	/*Boucle principale */
+		    	System.out.println("On entre dans la boucle principale");
+		    	/*Maintenant on va transmettre les donnes */
+		    	String donnees3;
+				
+				int taille3 = 0;
+				
+				try {
+					socket.receive(paquet3);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// message du premier client 1 
+				System.out.println("\n"+paquet3.getAddress());
+				taille3 = paquet3.getLength();
+				donnees3 = new String(paquet3.getData(),0, taille3);
+				System.out.println("Donnees reçues = "+donnees3);
+
+		    	/*On evalue de qui vient le premier message que on recoit */
+		    	 String regex="[,]";
+		         String[] en_tete = donnees3.split(regex);
+		         System.out.println("La chaine split");
+		         
+		         for(int k=0;k<en_tete.length;k++){
+		        	 System.out.println("split "+k+" "+en_tete[k]);
+		         }
+		         System.out.println("En tete :"+en_tete[0]+"!=1");
+		         
+		         if(en_tete[0].equals("1")){
+		        	 /*On a recu le paquet du client 1*/
+		        	 //transmission du message au client 3
+		        	 System.out.println("On doit envoyer au client 3 ");
+		        	 envoi = new DatagramPacket(donnees3.getBytes(), 
+		        			 donnees3.length(), paquet2.getAddress(), paquet2.getPort());
+        		     try {
+						socket.send(envoi);
+					 }  catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					 }
+		         }else{
+		        	 /*On a recu le paquet du client 3*/
+		        	 System.out.println("On doit envoyer au client 1 ");
+		        	 //transmission du message au client 1
+		        	 envoi = new DatagramPacket(donnees3.getBytes(), 
+		        			 donnees3.length(), paquet.getAddress(), paquet.getPort());
+        		     try {
+						socket.send(envoi);
+					 } catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					 }
+		         }
+		    }
+		  
 	}
 	
 	
-	
+	//ok
 	public void Client3(){
 		 try {
-			 
+			  System.out.println("CLIENT 3 ");
 			  String a_envoyer ="3,Bonjour test3";  
 		      InetAddress serveur = InetAddress.getByName("localhost");
 		      int length = a_envoyer.length();
@@ -177,12 +281,25 @@ public class ButtonClient1 extends JButton implements MouseListener {
 		      DatagramPacket donneesRecues = new DatagramPacket(new byte[taille], taille);
 		      socket.setSoTimeout(30000);
 		      socket.send(donneesEmises);
+		      System.out.println("On attend confirmation");
 		      socket.receive(donneesRecues);
 
-		      System.out.println("Message : " + new String(donneesRecues.getData(), 
-		        0, donneesRecues.getLength()));
-		      System.out.println("de : " + donneesRecues.getAddress() + ":" + 
-		        donneesRecues.getPort());
+		      System.out.println("Message : " + new String(donneesRecues.getData(),0, donneesRecues.getLength()));
+		      System.out.println("de : " + donneesRecues.getAddress() + ":" + donneesRecues.getPort());
+		      //effectuer envoie apres confirmation pret 
+		      donneesRecues = new DatagramPacket(new byte[taille], taille);
+		      System.out.println("Message 2 :  ");
+		      socket.receive(donneesRecues);
+		      System.out.println("Message : " + new String(donneesRecues.getData(), 0, donneesRecues.getLength()));
+		      System.out.println("de : " + donneesRecues.getAddress() + ":" + donneesRecues.getPort());
+		      a_envoyer="3,Message de test transmission venant de 3 ";
+		      length = a_envoyer.length();
+		      buffer = a_envoyer.getBytes();
+		      donneesEmises = new DatagramPacket(buffer, length, serveur, port);
+		      Thread.sleep(5000);
+		      socket.send(donneesEmises);
+		      System.out.println("On a envoyer le message ");
+		      System.out.println("Terminaison  ");
 		    } catch (SocketTimeoutException ste) {
 		      System.out.println("Le delai pour la reponse a expire");
 		    } catch (Exception e) {
